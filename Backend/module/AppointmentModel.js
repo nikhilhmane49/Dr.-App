f54e87b51ef13fd2350b42acb76f468332e0f628
nikhilhmane49 <manenikhil1949@gmail.com>
2024-11-08 23:03:10 +0530

    m

---
+++ Backend/module/AppointmentModel.js
@@ -0,0 +1,56 @@
+
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+
+    userId: {
+        type: String,
+        required: true
+    },
+    docId: {
+        type: String,
+        required: true
+    },
+    slotDate: {
+        type: String,
+        required: true
+    },
+    slotTime: {
+        type: String,
+        required: true
+    },
+    userData: {
+        type: Object,
+        require:true
+    },
+    docData: {
+        type: Object,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Number,
+        required: true
+    },
+    cancelled: {
+        type: Boolean,
+        default: false
+    },
+    payment: {
+        type: Boolean,
+        default: false
+    },
+    iscompleted: {
+        type: Boolean,
+        default: false
+    }
+
+
+});
+
+const AppointmentModel = mongoose.model('Appointment',appointmentSchema);
+
+module.exports = AppointmentModel;
